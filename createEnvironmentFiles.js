a1f512526da6a26500b3fcf3a7d072b49732dac6
Joachim Pihlgren <j.w.pihlgren@gmail.com>
2022-01-22 13:44:02 +0100

    Add script to create environment vars on build

---
+++ createEnvironmentFiles.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = "src/environments";
+const prodFile = "environment.prod.ts";
+
+const content = `${process.env.ENVIRONMENT_VARIABLES}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+    if(err) {
+        console.log(`src doesn't exist, creating it now ${process.cwd()}`);
+        fs.mkdir(dir, {recursive: true}, (err) => {
+            if (err) throw err;
+        });
+    }
+
+    try {
+        fs.writeFileSync(`${dir}/${prodFile}`, content);
+        console.log(`Created successfully in ${process.cwd()}`);
+        if(fs.existsSync(`${dir}/${prodFile}`)) {
+            console.log(`File is created ${path.resolve(`${dir}/${prodFile}`)}`);
+            const string = fs.readFileSync(`${dir}/${prodFile}`).toString();
+            console.log(str)
+        }
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})
