b3ef557c1cbc22637db0df9fbac7dbbebd868874
Joachim Pihlgren <j.w.pihlgren@gmail.com>
2022-01-23 18:57:42 +0100

    refact: bad path

--- src/app/modules/detailed-movie/detailed-movie.module.ts
+++ src/app/modules/detailed-movie/detailed-movie.module.ts
@@ -1,4 +1,4 @@
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { DetailedMovieRoutingModule } from './detailed-movie-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
