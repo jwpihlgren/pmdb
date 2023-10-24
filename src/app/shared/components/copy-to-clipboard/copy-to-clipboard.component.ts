b751b61f7048719096ba8a4b6cff85fb6d55486e
Joachim Pihlgren <j.pihlgren@outlook.com>
2023-10-24 21:00:11 +0200

    add copy to clipboard button

---
+++ src/app/shared/components/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -0,0 +1,34 @@
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { CopyToClipboardService } from '../../services/copy-to-clipboard.service';
+
+@Component({
+  selector: 'app-copy-to-clipboard',
+  templateUrl: './copy-to-clipboard.component.html',
+  styleUrls: ['./copy-to-clipboard.component.css']
+})
+export class CopyToClipboardComponent implements OnInit, AfterViewInit {
+
+  constructor(
+    private elt:ElementRef, 
+    private copyToClipboardService: CopyToClipboardService) { }
+
+  copyToClipboardIsSupported!: boolean
+  isCopied: boolean = false
+  content!: string
+
+  ngOnInit(): void {
+    this.copyToClipboardIsSupported = this.copyToClipboardService.isSupported();
+  }
+
+  ngAfterViewInit(): void {
+    this.content = this.elt.nativeElement.childNodes[0].innerText
+  }
+
+  copy(): void {
+    if (!this.copyToClipboardIsSupported) return;
+    this.copyToClipboardService.copy(this.content);
+    this.isCopied = true;
+    setTimeout(() => this.isCopied = false, 2000)
+  }
+
+}
